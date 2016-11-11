783b7eaaa6fcbf64dca7bcebd6e32dec64eb4cc0
sorney <dave.sorenson@gmail.com>
2016-11-11 13:37:40 -0600

    Added data table test

--- src/test/groovy/Specs/HelloWorldSpec.groovy
+++ src/test/groovy/Specs/HelloWorldSpec.groovy
@@ -5,6 +5,7 @@ import PageObjects.ExampleHomePage
 import PageObjects.HelloWorldPage
 import spock.lang.Issue
 import spock.lang.Narrative
+import spock.lang.Unroll
 
 @Narrative("Spec for walking thru the Hello World Example")
 class HelloWorldSpec extends BaseSpec {
@@ -27,4 +28,31 @@ class HelloWorldSpec extends BaseSpec {
         then:"Assert it takes"
             assert getLiveExampleResultText() == "Hello, Firsty McLastName!"
     }
+
+
+    @Unroll
+    @Issue("Hello World Example, but with spock data tables")
+    def HelloWorldExamplePageDataTable() {
+        when: "At the home page, click hello world link"
+            at ExampleHomePage
+            clickHelloWorldLink()
+        then: "Verify at the Hello World Page"
+            assert at(HelloWorldPage)
+        when:"Enter names in first and last name field"
+            setNameTextBoxValues(firstName, lastName)
+        then:"Assert it takes"
+            assert getLiveExampleResultText() == "Hello, "+firstName+" "+lastName+"!"
+        where:
+        firstName   | lastName
+        "Firsty"    |  "McLastName"
+        "Peter"     |  "Griffin"
+        "Lois"      |  "Griffin"
+        "Glen"      |  "Quagmire"
+        "Bonnie"    |  "Swanson"
+        "Cleveland" | "Brown"
+
+    }
+
+
+
 }
